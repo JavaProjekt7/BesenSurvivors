9bfee2542d6610b7303527df20fabc2ca2195000
Choso07 <adrian.braun07@gmx.de>
2024-12-01 13:13:08 +0100

    einstellung endgame  button usw

--- src/main/java/besensurvivors/core/SceneGenerator.java
+++ src/main/java/besensurvivors/core/SceneGenerator.java
@@ -1,4 +1,5 @@
 package besensurvivors.core;
+
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,37 +9,75 @@ import javafx.stage.Stage;
 
 
 public class SceneGenerator {
-  public Scene generateStartWindow() {
-      // Die Objekte erstellen, in denen alle Elemente sind
-      VBox root = new VBox();
-      Scene scene = new Scene(root);
+    public Scene generateStartWindow() {
+        VBox root = new VBox(20);
+        Scene scene = new Scene(root, 400, 300);
 
-      // Label erstellen, der den Titel beinhaltet
-      Label label = new Label("BesenSurvivors");
-      label.setStyle("-fx-font-size: 37px;");
-      root.getChildren().add(label);
+        Label label = new Label("BesenSurvivors");
+        label.setStyle("-fx-font-size: 37px;");
+        root.getChildren().add(label);
 
-      // Button für das Spiel starten erstellen
-      Button startGameButton = new Button();
-      startGameButton.setText("Start Game");
-      startGameButton.setPrefHeight(50);
-      startGameButton.setPrefWidth(200);
-      root.getChildren().add(startGameButton);
+        Button startGameButton = new Button("Start Game");
+        startGameButton.setText("Start Game");
+        startGameButton.setPrefHeight(50);
+        startGameButton.setPrefWidth(200);
+        root.getChildren().add(startGameButton);
 
-      // Button für das beenden des Spiels
-      Button endGameButton = new Button();
-      endGameButton.setText("Endgame");
-      endGameButton.setPrefHeight(50);
-      endGameButton.setPrefWidth(200);
-      root.getChildren().add(endGameButton);
-      endGameButton.setOnAction(event -> ((Stage) endGameButton.getScene().getWindow()).close());
+        //Einstellungs button
+        Button settingsButton = new Button("Einstellungen");
+        settingsButton.setPrefHeight(50);
+        settingsButton.setPrefWidth(200);
+        root.getChildren().add(settingsButton);
 
 
+        // Button zum beenden des Spiels
+        Button endGameButton = new Button();
+        endGameButton.setText("Endgame");
+        endGameButton.setPrefHeight(50);
+        endGameButton.setPrefWidth(200);
+        root.getChildren().add(endGameButton);
 
 
-      // Den root (VBox) modifizieren
-      root.setAlignment(Pos.CENTER);
-      root.setStyle("-fx-background-color: #bb33ff");
-    return scene;
+        endGameButton.setOnAction(event -> ((Stage) endGameButton.getScene().getWindow()).close());
+        settingsButton.setOnAction(event -> openSettingsWindow());
+
+        root.setAlignment(Pos.CENTER);
+        root.setStyle("-fx-background-color: #bb33ff");
+
+        return scene;
+    }
+
+    private void openSettingsWindow() {
+        Stage settingsStage = new Stage();
+        settingsStage.setTitle("Einstellungen");
+
+        VBox settingsRoot = new VBox(20);
+        settingsRoot.setAlignment(Pos.CENTER);
+
+
+        // Lautstärke-Label
+        Label volumeLabel = new Label("Lautstärke:");
+        volumeLabel.setStyle("-fx-font-size: 18px;");
+
+        // Lautstärke-Slider zum Lautstärke ändern
+        javafx.scene.control.Slider volumeSlider = new javafx.scene.control.Slider(0, 100, 50);
+        volumeSlider.setShowTickLabels(true);
+        volumeSlider.setShowTickMarks(true);
+        volumeSlider.setMajorTickUnit(25);
+
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            double volume = newValue.doubleValue() / 100;
+            System.out.println("Lautstärke: " + volume);
+
+        });
+
+        // Zurück-Button
+        Button backButton = new Button("Zurück");
+        backButton.setOnAction(event -> settingsStage.close());
+
+        settingsRoot.getChildren().addAll(volumeLabel, volumeSlider, backButton);
+        Scene settingsScene = new Scene(settingsRoot, 300, 200);
+        settingsStage.setScene(settingsScene);
+        settingsStage.show();
     }
 }
