f6a96b8658e565fa6160b70adf4cebf76d5dce6e
User <oguzkan8@icloud.com>
2025-01-12 21:13:25 +0100

    code start neu

---
+++ resources/Musik.java
@@ -0,0 +1,3 @@
+
+[InternetShortcut]
+URL=https://cdn.discordapp.com/attachments/1168197982848553050/1328047417282527294/BeepBox-Song.wav?ex=6785480a&is=6783f68a&hm=3c17b3307b3632df96c070d6e424c22904bb130c0761ab36633122ea5550dfda&
---
+++ src/main/java/besensurvivors/core/Musik.java
@@ -0,0 +1,38 @@
+package besensurvivors.core;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
+import java.io.File;
+
+public class Musik {
+
+    private MediaPlayer mediaPlayer;
+
+
+    public void playMusic(String filePath) {
+        try {
+
+            Media media = new Media(new File(filePath).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+
+            mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
+            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+
+            // Musik starten
+            mediaPlayer.play();
+            System.out.println("Musik gestartet: " + filePath);
+        } catch (Exception e) {
+            System.err.println("Fehler beim Abspielen der Musik: " + e.getMessage());
+        }
+    }
+
+
+    public void stopMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            System.out.println("Musik gestoppt.");
+        }
+    }
+}
